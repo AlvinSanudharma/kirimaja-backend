1bf75706292a003e68c9543c2a2b97c3f70f3014
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-11-11 20:33:15 +0800

    API Login 1

---
+++ src/modules/auth/dto/auth-login.dto.ts
@@ -0,0 +1,27 @@
+import { z, ZodObject } from 'zod';
+
+const authLoginSchema = z.object({
+    email: z
+        .string({
+            required_error: 'Email is required',
+            invalid_type_error: 'Email must be a string',
+        })
+        .email({
+            message: 'Invalid format',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+            invalid_type_error: 'Password must be a string',
+        })
+        .min(8, 'password must be at least 8 characters long'),
+});
+
+export class AuthLoginDto {
+    static schema: ZodObject<any> = authLoginSchema;
+
+    constructor(
+        public readonly email: string,
+        public readonly password: string,
+    ) {}
+}
---
+++ src/modules/auth/response/auth-login.response.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Expose, Type } from 'class-transformer';
+
+class RoleResponse {
+    @Expose()
+    id: number;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    key: string;
+
+    @Expose()
+    @Type(() => PermissionResponse)
+    permissions: PermissionResponse[];
+}
+
+class PermissionResponse {
+    @Expose()
+    id: number;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    key: string;
+
+    @Expose()
+    description: string;
+}
+
+class UserResponse {
+    @Expose()
+    id: number;
+
+    @Expose()
+    email: number;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    avatar: string;
+
+    @Expose()
+    phoneNumber: string;
+
+    @Expose()
+    @Type(() => RoleResponse)
+    roles: RoleResponse[];
+}
+
+export class AuthLoginResponse {
+    @Expose()
+    accessToken: string;
+
+    @Expose()
+    @Type(() => UserResponse)
+    user: UserResponse[];
+}
