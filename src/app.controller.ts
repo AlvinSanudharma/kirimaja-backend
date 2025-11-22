b6a7084600ddf76b927e57297b98a52f57b1c0c0
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-11-22 18:45:11 +0800

    Guard Permission 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './modules/auth/guards/logged-in.guard';
+import { PermissionGuard } from './modules/auth/guards/permission.guard';
+import { RequirePermissions } from './modules/auth/decorators/permissions.decorator';
 
 @Controller()
+@UseGuards(JwtAuthGuard, PermissionGuard)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Get('protected')
+    @RequirePermissions('shipments.create')
+    getProtectedResource(): string {
+        return 'This is a protected resource';
+    }
 }
